7c52d127c395532dd386f48f678f7794b7976f03
Onkar Mali <onkarmali@Onkars-MacBook-Air.local>
2025-06-27 10:28:34 +0530

    all files

--- src/test/java/com/test/SeleniumActionsTest.java
+++ src/test/java/com/test/SeleniumActionsTest.java
@@ -1,6 +1,11 @@
 import java.time.Duration;
+import java.util.List;
 
+import static org.apache.commons.lang3.ObjectUtils.wait;
+import org.bouncycastle.jcajce.provider.asymmetric.EC;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -29,5 +34,51 @@ public class SeleniumActionsTest extends BaseTest {
 
         }
       
+    }
+
+    @Test(priority=3)
+    void test_registraction_form() throws InterruptedException{
+
+        driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Onkar");
+        driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("Mali");
+        
+        
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("onkar@test.com");
+        driver.findElement(By.xpath("//input[@type='tel']")).sendKeys("9876543210");
+        driver.findElement(By.xpath("//label[normalize-space()='Male']")).click();
+
+        List<WebElement> checkboxes = driver.findElements(By.xpath("//*[@type = 'checkbox']"));
+        System.err.println(checkboxes.size());
+        for(int i =0;i < checkboxes.size(); i++){
+            if (!checkboxes.get(i).isSelected()) {
+                checkboxes.get(i).click();
+            }
+            
+        }
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,1000)");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement countryContainer = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='select2-selection select2-selection--single']")));
+        countryContainer.click();
+
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//*[@class='select2-search__field']")).sendKeys("India");
+        driver.findElement(By.xpath("//*[@class='select2-results__option select2-results__option--highlighted']")).click();
+
+       Thread.sleep(2000);
+    }
+
+    @Test
+    void test_automcomplete() throws InterruptedException{
+
+        try {
+
+            driver.get("https://demo.automationtesting.in/AutoComplete.html");
+            driver.findElement(By.xpath("//*[@class='ui-autocomplete-multiselect ui-state-default ui-widget']")).sendKeys("India");
+            Thread.sleep(500);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }
--- src/test/java/com/test/readFilesTest.java
+++ src/test/java/com/test/readFilesTest.java
@@ -1,13 +1,20 @@
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
 
 class readFilesTest{
 
     String read_properties_file(String path , String fieldName){
         
-        Properties prop = new Properties();
         try{
+            Properties prop = new Properties();
             FileInputStream fis = new FileInputStream(path);
             prop.load(fis);
             return prop.getProperty(fieldName);        
@@ -15,5 +22,15 @@ class readFilesTest{
             System.err.println(e);
             return null;
         }     
+    }
+    @Test
+    void read_excel(String path) throws FileNotFoundException, IOException{
+        FileInputStream fis = new FileInputStream(path);
+        Workbook workbook =new XSSFWorkbook(fis);
+        Sheet sheet = workbook.getSheet("Test");
+
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        int colCount = sheet.getRow(0).getPhysicalNumberOfCells();
+
     }
 }
